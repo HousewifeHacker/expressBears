d362d524ea3d8bdc6ab6768fa5dcb0adf4191016
housewifehacker <housewifehacker@gmail.com>
2018-06-12 11:46:26 -0500

    Can use es6 features

--- server.js
+++ server.js
@@ -1,29 +1,29 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
-var port = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
 
-var router = express.Router();
-
-// register routes
+// tester route, GET only
 app.get('/', function(req, res) {
-    res.send({ message: 'hooray! welcome to our api!' });   
-});
-router.get('/', function(req, res) {
-    res.send({ message: 'Welcome to router!'});
+    res.json({ message: 'hooray!' });
 });
 
-var tryRouter = express.Router();
-tryRouter.get('/lala', function(req, res) {
-    res.send({ hi: 'hello' }); 
+
+var apiRouter = express.Router();
+// use next to use additional routes on apiRouter
+apiRouter.use( (req, res, next) => {
+    next();
 });
+apiRouter.get('/', (req, res) => {
+    res.json({ message: 'Our API' })
+})
 
 // router, like flask blueprint
 // prefixes route with '/api'
-app.use('/api', router);
-app.use('/lala', tryRouter);
+// 'app.use' is Middlewear so app can be used by get method
+app.use('/api', apiRouter);
 
 // start server
-app.listen(port);
-console.log('Magic happens on port ' + port);
+app.listen(PORT);
+console.log('Magic happens on port ' + PORT);
 
